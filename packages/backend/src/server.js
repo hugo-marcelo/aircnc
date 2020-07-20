56f643da2be6371e62681f4dbd315dca22bc32b3
hugo-marcelo <hugo.marcelo@lg.com.br>
2020-07-20 10:09:41 -0300

    chore: configured backend to upload to heroku

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const socketio = require('socket.io');
-const http = require('http');
+const socketio = require("socket.io");
+const http = require("http");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-mongoose.connect('mongodb+srv://rocketseat:rocketseat@cluster0-umivl.mongodb.net/aircnc?retryWrites=true&w=majority', {
+mongoose.connect(
+  "mongodb+srv://rocketseat:rocketseat@cluster0-umivl.mongodb.net/aircnc?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 const connectedUsers = {};
 
-io.on('connection', socket => {
-    const {user_id} = socket.handshake.query;
+io.on("connection", (socket) => {
+  const { user_id } = socket.handshake.query;
 
-    connectedUsers[user_id] = socket.id;
+  connectedUsers[user_id] = socket.id;
 });
 
 app.use((req, res, next) => {
-    req.io = io;
-    req.connectedUsers = connectedUsers;
+  req.io = io;
+  req.connectedUsers = connectedUsers;
 
-    return next();
+  return next();
 });
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
